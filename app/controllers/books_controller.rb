214f141753fa5fe6f8dbc37db03d0c19ebdb5061
Rengemaru <t5424035@fukuyama-u.ac.jp>
2025-09-06 07:54:12 +0000

    [Feat] バリデーションの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,8 @@ class BooksController < ApplicationController
       flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
-      render :new
+      @books = Book.all
+      render :index
     end
   end
 
@@ -25,10 +26,10 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      flash[:notice] = "Book was successfully created."
+      flash[:notice] = "Book was successfully updated."
       redirect_to book_path(@book.id)
     else
-      render :update
+      render :edit
     end
   end
 
